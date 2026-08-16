1e0a1f0020d34e8b614da230959be08c423cfc2b
Kaksoisvirhe <47853366+Kaksoisvirhe@users.noreply.github.com>
2026-08-16 14:59:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Gate from "./Gate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Gate>
+      <App />
+    </Gate>
   </React.StrictMode>
 );
